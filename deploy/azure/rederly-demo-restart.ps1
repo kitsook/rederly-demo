b2551d26f7408a1e9d2875131fd805abae86db2a
Clarence Ho <clarenceho@gmail.com>
2020-12-21 19:40:52 -0800

    Add powershell script for restarting
    - add powershell script for restarting deployment on Azure

---
+++ deploy/azure/rederly-demo-restart.ps1
@@ -0,0 +1,21 @@
+
+# To run this script, your Azure Automation Account needs to has the "AzureRM.Websites" module installed.
+# The easiest way to do this is go to PowerShell Gallery at https://www.powershellgallery.com/packages/AzureRM.Websites/5.2.0 ,
+# open the tab "Azure Automation" and clik "Deploy to Azure Automation" button
+#
+# This script is copied from https://stackoverflow.com/questions/47298375/how-to-restart-an-azure-appservice-from-azure-powershell
+# Update "the-subscription-id", "your-resource-group-name", and "your-application-name" accordingly.
+#
+$connectionName = "AzureRunAsConnection" # this is the default connection created when you provision the Automation account,
+                                         # you might need to change this to your own connection name
+$servicePrincipalConnection = Get-AutomationConnection -Name $connectionName
+
+$null = Add-AzureRmAccount `
+    -ServicePrincipal `
+    -TenantId $servicePrincipalConnection.TenantId `
+    -ApplicationId $servicePrincipalConnection.ApplicationId `
+    -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint
+
+$null = Select-AzureRmSubscription -SubscriptionId 'the-subscription-id' ` # Needed if you have more than 1 subscription
+
+Restart-AzureRmWebApp -ResourceGroupName your-resource-group-name -Name your-application-name
